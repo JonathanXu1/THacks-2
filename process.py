160d2ccf00466580c6e0afef791d8c972a6808e2
mdu02 <32987869+mdu02@users.noreply.github.com>
2017-10-22 08:37:27 -0400

    removed files

--- process.py
+++
@@ -1,8 +0,0 @@
-import serial as serial
-
-arduino = serial.Serial('COM3', 9600, timeout=.1)
-
-while True:
-        data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
-        if data:
-                print (data)
