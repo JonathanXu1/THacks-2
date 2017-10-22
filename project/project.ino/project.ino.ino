e9f7c8fe7a4ba13cff67df52caa16b1059420a07
Jonathan Xu <jonathanlikestoemail@gmail.com>
2017-10-22 08:05:29 -0400

    A lot of things
    Cust

--- project/project.ino/project.ino.ino
+++ project/project.ino/project.ino.ino
@@ -1,32 +1,45 @@
 /*The servo program
  *Moves the servo to a variety of positions
- *Jonathan Xu
- *January 25, 2017
 */
 
-const int trigPin = 8;
-const int echoPin = 9;
+#include<Servo.h>
+Servo base;
+Servo pull;
+
+const int trigPin = 7;
+const int echoPin = 6;
 
 long duration;
 long distance;
 
 void setup() {
   // put your setup code here, to run once:
-  
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  base.attach(10);
+  pull.attach(9);
+  pull.write(0);
 
-  pinMode(trigPin, 7);
-  pinMode(echoPin, 6);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    Serial.print("Distance: ");
-    Serial.print(ping());
-    Serial.println("cm");
+  for (int x=0; x<=150; x+=5) {
+      for (int y=0; y<=180; y+=5) {
+      base.write(y);
+      Serial.print("Base position: ");
+      Serial.println(y);
+      Serial.print("Arm position: ");
+      Serial.println(x);
+      Serial.print("Distance: ");
+      Serial.print(ping());
+      Serial.println("cm");
+      delay(250);
+    }
+    pull.write(x);
     delay(250);
+  }
 }
 
 int ping() {
